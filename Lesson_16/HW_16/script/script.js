11171c2ae63e04e5164850d21543a48d76347eb5
AlexAlimow <gorlum007rus@gmail.com>
2025-08-27 10:12:20 +0200

    HW_16 uptade

--- Lesson_16/HW_16/script/script.js
+++ Lesson_16/HW_16/script/script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const curtain = document.getElementById("curtain");
+    const curtain = document.getElementById("curtain");
 
-  let currentTop = 0;            // стартовое положение
-  const step = 10;               // на сколько % опускаем за шаг
-  const interval = 1000;         // каждая секунда
+    let currentTop = 0;            // стартовое положение
+    const step = 10;               // на сколько % опускаем за шаг
+    const interval = 1000;         // каждая секунда
 
-  const timer = setInterval(() => {
-    currentTop += step;
-    if (currentTop >= 100) {
-      currentTop = 100;
-      clearInterval(timer);
-    }
-    curtain.style.top = currentTop + "%";
-  }, interval);
+    const timer = setInterval(() => {
+        currentTop += step;
+        if (currentTop >= 100) {
+            currentTop = 100;
+            clearInterval(timer);
+        }
+        curtain.style.top = currentTop + "%";
+    }, interval);
 });
